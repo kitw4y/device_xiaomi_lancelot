2be84c5ada9e745b747e4c362b2f21dc3727a7e5
kitw4y <91876095+kitw4y@users.noreply.github.com>
2024-03-05 17:21:54 +0700

    bringup

--- rising_lancelot.mk
+++ rising_lancelot.mk
@@ -11,14 +11,20 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device makefile.
 $(call inherit-product, device/xiaomi/lancelot/device.mk)
 
-# SuperiorOS
-SUPERIOR_OFFICIAL := true
-SUPERIOR_GAPPS := minimal
+# Inherit some common RisingOS stuff.
+$(call inherit-product, vendor/rising/config/rising.mk)
+
+# RisingOs Flags
+RISING_MAINTAINER := Lang
+RISING_CHIPSET := MT6768
+WITH_GMS := true
 TARGET_BOOT_ANIMATION_RES := 1080
-
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/superior/config/common.mk)
-
+RISING_BATTERY := 5020mAh
+RISING_DISPLAY := 2340x1080
+RISING_BUILDTYPE := UNOFFICIAL
+TARGET_PREBUILT_GOOGLE_CAMERA := true
+TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
 PRODUCT_NAME := superior_lancelot
 PRODUCT_DEVICE := lancelot
 PRODUCT_MANUFACTURER := Xiaomi
